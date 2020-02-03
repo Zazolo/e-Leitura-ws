736a5e372ab01db17c3be518fc78efe579c0ddd7
Tiago Batista Corrêa <tiagobc@live.com>
2020-02-03 11:55:53 -0300

    Initial Commit

---
+++ src/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+        return $response->withJson(["teste" => $args['name']], 200);
+    });
+
+};
